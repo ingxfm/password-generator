df597b422846452d1be4a5ce28d22bd316ad64c4
Freddy B <52885763+ingxfm@users.noreply.github.com>
2022-02-02 22:21:22 +0100

    Update passgen.py

--- passgen.py
+++ passgen.py
@@ -1,4 +1,4 @@
-import random
+from random import choice
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -16,13 +16,13 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 password_list = []
 for letteri in range(nr_letters):
-  password_list += random.choice(letters)  
+  password_list += choice(letters)  
 
 for symboli in range(nr_symbols):
-  password_list += random.choice(symbols)
+  password_list += choice(symbols)
 
 for numberi in range(nr_numbers):
-  password_list += random.choice(numbers)
+  password_list += choice(numbers)
 
 random.shuffle(password_list)
 
